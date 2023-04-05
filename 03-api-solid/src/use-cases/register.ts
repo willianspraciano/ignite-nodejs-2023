11afad927a99a9cd7dd018876769e75eecda204c
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-04-05 17:23:29 -0300

    03: repository interface

--- 03-api-solid/src/use-cases/register.ts
+++ 03-api-solid/src/use-cases/register.ts
@@ -1,7 +1,6 @@
 import { hash } from 'bcryptjs';
 
-import { prisma } from '@/lib/prisma';
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository';
+import { UsersRepository } from '@/repositories/users-repository';
 
 interface IRequest {
   name: string;
@@ -10,14 +9,12 @@ interface IRequest {
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ name, email, password }: IRequest) {
     const password_hash = await hash(password, 6);
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: { email },
-    });
+    const userWithSameEmail = await this.usersRepository.findByEmail(email);
 
     if (userWithSameEmail) {
       throw new Error('E-mail already exists.');
