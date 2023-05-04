8e8fc650cfcbe999b14960960c001e7394317ee6
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-04 17:18:41 -0300

    03: use case factories

---
+++ 03-api-solid/src/use-cases/factories/make-check-in-use-case.ts
@@ -0,0 +1,12 @@
+import { CheckInUseCase } from '../check-in';
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository';
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository';
+
+export function makeCheckInUseCase() {
+  const checkInRepository = new PrismaCheckInsRepository();
+  const gymsRepository = new PrismaGymsRepository();
+
+  const useCase = new CheckInUseCase(checkInRepository, gymsRepository);
+
+  return useCase;
+}
---
+++ 03-api-solid/src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
@@ -0,0 +1,10 @@
+import { FetchUserCheckInsHistoryUseCase } from '../fetch-user-check-ins-history';
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository';
+
+export function makeFetchNearbyGymsUseCase() {
+  const checkInsRepository = new PrismaCheckInsRepository();
+
+  const useCase = new FetchUserCheckInsHistoryUseCase(checkInsRepository);
+
+  return useCase;
+}
