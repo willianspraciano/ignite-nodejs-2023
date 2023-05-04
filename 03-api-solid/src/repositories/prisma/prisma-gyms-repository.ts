d09d11fcdd70ae56ed8cd2d6f2bd33afeb470316
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-04 16:10:56 -0300

    03: prisma gyms repository

---
+++ 03-api-solid/src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,39 @@
+import { Gym, Prisma } from '@prisma/client';
+
+import { prisma } from '@/lib/prisma';
+import { GymsRepository, IFindManyNearby } from './../gyms-repository';
+
+export class PrismaGymsRepository implements GymsRepository {
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    });
+
+    return gym;
+  }
+
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({ where: { id } });
+
+    return gym;
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: { contains: query },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+
+    return gyms;
+  }
+
+  async findManyNearby({ latitude, longitude }: IFindManyNearby) {
+    const gyms = await prisma.$queryRaw<Gym[]>`SELECT * from gyms
+    WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10`;
+
+    return gyms;
+  }
+}
